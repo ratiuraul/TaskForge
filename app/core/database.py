de0f88f31838213578f74e211a1a7dd962b3b753
Raul Ratiu <raulratiu@Rauls-MacBook-Pro.local>
2026-07-06 21:01:23 +0300

    add user sql model

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
---
+++ app/modules/auth/models/user_model.py
@@ -0,0 +1,11 @@
+from app.core.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[str]
+    hashed_password: Mapped[str]
