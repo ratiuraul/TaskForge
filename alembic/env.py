97517b9a1b19b44a656f5f9687d2f1738d103237
Raul Ratiu <raulratiu@Rauls-MacBook-Pro.local>
2026-07-07 09:19:02 +0300

    update alembic env for user model

--- alembic/env.py
+++ alembic/env.py
@@ -4,11 +4,16 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from app.core.base import Base
-
+from app.core.database import Base
+from app.core.config import settings
+from app.modules.auth import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,6 +25,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+print(target_metadata.tables.keys())
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
