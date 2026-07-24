864989c6b5f9c7f80c7cd8c89cab63338033f412
Raul Ratiu <raulratiu@Rauls-MacBook-Pro.local>
2026-07-24 15:29:40 +0300

    add initial CI pipeline

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,6 @@ class Settings(BaseSettings):
     DATABASE_URL: str 
     JWT_SECRET: str
     REDIS_URL: str
-    JWT_SECRET: str
     JWT_ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     TEST_DATABASE_URL: str
